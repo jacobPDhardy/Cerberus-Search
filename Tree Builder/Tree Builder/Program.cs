9eec7585dfffe2addfedf286c099f2394751a833
Jacob Hardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-01-18 16:31:34 +0000

    Upload of Tree Builder
    This program parses the top level statement into a tree of operations. I need to add a variation of my escape sequence algorithm to this to prevent it breaking when user uses one bracket. Also the solve logic needs completing.

---
+++ Tree Builder/Tree Builder/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tree_Builder;
+
+SearchStatement search = new SearchStatement("(a AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)");
+SearchStatement search2 = new SearchStatement("(\"a()\" AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)");
+SearchStatement search3 = new SearchStatement("(\"a(\" AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)"); //NEED TO ADD ESCAPE ALGORITHM TO TOP LEVEL TRANSLATOR
