b0891c2445c1f708f3c913b426077b067a8673e2
Jacob Hardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-01-18 16:26:23 +0000

    Upload of Gate Solver again (Last upload was wrong)
    This program performs the gate operations on the datasets. Database needs adding if you want to use it. Also additionally testing is required and I believe it can be sped up by removing some unnecessary sorts.

---
+++ Gate Solver/Gate Solver/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gate_Solver
+{
+    public class Log
+    {
+        public int Id { get; private set; }
+        public DateTime TimeStamp { get; private set; }
+        public Levels Level { get; private set; }
+        public string Exception { get; private set; }
+        public string RenderedMessage { get; private set; }
+        public string Properties { get; private set; }
+
+        public Log(string id, string timeStamp, string level,string exception,string renderedMessage,string properties)
+        {
+            Id = Int32.Parse(id);
+            TimeStamp = DateTime.Parse(timeStamp);
+            Level = (Levels)Enum.Parse(typeof(Levels), level);
+            Exception = exception;
+            RenderedMessage = renderedMessage;  
+            Properties = properties;
+        }
+
+        public override string ToString()
+        {
+            return $"Id={Id}\nTimeStamp:{TimeStamp}\nLevel:{Level}\nException:{Exception}\nRenderedMessage:{RenderedMessage}\nProperties:{Properties}\n";
+        }
+    }
+}
