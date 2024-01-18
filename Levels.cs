77c6a2beff630e1b43b2ea8143091c9463e61da5
Jacob Hardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-01-18 16:11:35 +0000

    Upload of Gate Solver
    This program performs the gate operations on the datasets. Database needs adding if you want to use it. Also additional testing is required and I believe it can be sped up by removing some unnecessary sorts.

---
+++ Levels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gate_Solver
+{
+    public enum Levels
+    {
+        Information,
+        Debug,
+        Warning,
+        Error,
+        Fatal
+    }
+}
