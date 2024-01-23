6a22b3e9de616de3d6b2860f466dfd391c3a1528
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-01-23 10:25:01 +0000

    Added test to utilities

---
+++ Cerberus Search Complete/Cerberus Search Complete/CSearchUtilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cerberus_Search_Complete
+{
+    public static class CSearchUtilities //DONE
+    {
+        public static async Task OutputDataset(List<Log> dataset, int delay = 0)
+        {
+            foreach (var data in dataset)
+            {
+                Console.WriteLine(data);
+                await Task.Delay(delay);
+            }
+        }
+
+        public static async Task Test(string search = "(\"garbage\" & !\"Information\") ^ !(\"2023-11-16\" & \"drive stages synced\")")
+        {
+            SearchStatement searchStatement = new SearchStatement(search);
+            List<Log> results = await searchStatement.Solve();
+            await OutputDataset(results);
+        }
+
+        public static async Task<TimeSpan> Benchmark(string benchmarkSearch = "(\"mcr\" ^ \"garbage\" ^ \"information\" ^ \"identity\" ^ \"2024\")")
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            SearchStatement searchStatement = new SearchStatement(benchmarkSearch);
+            List<Log> results = await searchStatement.Solve();
+
+            timer.Stop();
+
+            TimeSpan timespan = timer.Elapsed;
+            Console.WriteLine($"{results.Count} results found in {timespan.ToString(@"m\:ss\.fff")}"); //Expected results = 53433
+            return timespan;
+        }
+
+        public static async Task BenchmarkAverage(string benchmarkSearch = "(\"mcr\" ^ \"garbage\" ^ \"information\" ^ \"identity\" ^ \"2024\")", int iterations = 10)
+        {
+            List<TimeSpan> times = new List<TimeSpan>();
+            for (int count = 0;count < iterations;count++)
+            {
+                Console.WriteLine($"Running test {count} out of {iterations}");
+                times.Add(await Benchmark(benchmarkSearch));
+            }
+            var total = times.Sum(time => time.TotalMicroseconds);
+            var average = total / iterations;
+            Console.WriteLine($"{iterations} tests ran in {total.ToString(@"m\:ss\.fff")} with an average test time of {average.ToString(@"m\:ss\.fff")}");
+        }
+    }
+}
--- Cerberus Search Complete/Cerberus Search Complete/Program.cs
+++ Cerberus Search Complete/Cerberus Search Complete/Program.cs
@@ -1,15 +1,4 @@
 ﻿using Cerberus_Search_Complete; //Done
 
-//string search = "(\"garbage\" & !\"Information\") ^ !(\"2023-11-16\" & \"drive stages synced\")";
-//string search2 = "\"halo\" & \"2024\"";
-//string search3 = "(\"garbage\" & !\"information\") ^ (\"2021-09-09T14:05:19\")";
-//string search4 = "(\"\")";
-//string search5 = "";
-////string ultimateTestSearch = "!(\"garbage\" | (!\"mcr\" & \"halo) | (!\"\"))";
-//string benchmarkSearch = "!((\"garbage\") ^ (\"halo\" ^ \"mcr\" ^ \"2023\") )";
-//SearchStatement searchStatement = new SearchStatement(benchmarkSearch);
-//await searchStatement.Solve();
-
-//await DatasetUtilities.OutputDataset(searchStatement.ResultDataset);
-
-await DatasetUtilities.BenchmarkAverage();
+await CSearchUtilities.Test();
+//await CSearchUtilities.BenchmarkAverage();
