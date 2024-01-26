f966e2bbf9e9ff401a94bf9057c685379a92578e
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2024-01-26 18:08:10 +0000

    Removed single function examples

--- Tree Builder/Tree Builder/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Tree_Builder;
-
-SearchStatement search = new SearchStatement("(a AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)");
-SearchStatement search2 = new SearchStatement("(\"a()\" AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)");
-SearchStatement search3 = new SearchStatement("(\"a(\" AND b) OR ((c XOR d) AND (NOT (e AND f))) OR (g AND h)"); //NEED TO ADD ESCAPE ALGORITHM TO TOP LEVEL TRANSLATOR
